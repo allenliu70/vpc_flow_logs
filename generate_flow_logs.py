ba2298ae01cf94e7e9b8868f6c97ab7734483582
Allen Liu <allenliu70@gmail.com>
2024-11-10 10:34:10 -0800

    In Synthetic flow logs generator: 1. randomized  protocol numbers 2. randomize dstports over a list of pre-selected port numbers.

--- generate_flow_logs.py
+++ generate_flow_logs.py
@@ -1,5 +1,39 @@
 import random
 import time
+import csv
+from typing import List
+
+def get_iana_protocol_numbers() -> List:
+    # Initialize an empty dictionary to store the mapping
+    protocol_numbers = []
+
+    try:
+        print('Retrieving IANA protocol numbers.')
+        # Open the CSV file containing the protocol numbers and names
+        with open('protocol-numbers.csv', mode='r') as file:
+            csv_reader = csv.reader(file)
+            
+            # Skip the header row if there is one
+            next(csv_reader)
+            
+            # Iterate over each row in the CSV file, assuming the first column is the protocol number
+            protocol_numbers = [int(row[0]) for row in csv_reader if row[0].isdigit()]
+                
+    except FileNotFoundError:
+        print("The file was not found.")
+    except IOError as e:
+        print(f"An I/O error occurred: {e}")
+    else:
+        print('Sucessfully read IANA protocol numbers from the saved csv file.')
+    finally:
+        print("Finished file operation.")
+    
+    return protocol_numbers
+
+# print(get_iana_protocol_numbers())
+iana_protocol_numbers = get_iana_protocol_numbers()
+
+pre_selected_ports = [25, 68, 23, 31, 443, 22, 3389, 0, 110, 993, 143]
 
 def generate_flow_log_entry():
     version = 2
@@ -8,8 +42,9 @@ def generate_flow_log_entry():
     srcaddr = f"192.168.{random.randint(0, 255)}.{random.randint(0, 255)}"
     dstaddr = f"203.0.{random.randint(0, 255)}.{random.randint(0, 255)}"
     srcport = random.randint(1024, 65535)
-    dstport = random.randint(1, 65535)
-    protocol = 6  # TCP
+    # dstport = random.randint(1, 65535)
+    dstport = random.choice(pre_selected_ports)
+    protocol = random.choice(iana_protocol_numbers) 
     packets = random.randint(1, 100)
     bytes = packets * random.randint(40, 1500)
     start = int(time.time())
@@ -20,7 +55,7 @@ def generate_flow_log_entry():
     return f"{version} {account_id} {eni_id} {srcaddr} {dstaddr} {srcport} {dstport} {protocol} {packets} {bytes} {start} {end} {action} {log_status}"
 
 # Generate a large number of flow log entries
-with open('synthetic_flow_logs.log', 'w') as file:
+with open('v2_synthetic_flow_logs.log', 'w') as file:
     for _ in range(10000):  # Adjust the number as needed
         file.write(generate_flow_log_entry() + "\n")
 
